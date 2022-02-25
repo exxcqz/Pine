f39b876699305a47fd19d890a14f53493b9fa5d9
Exxcqz <electro.style72@gmail.com>
2022-02-25 19:58:41 +0600

    add-MainCollection-and-Fonts

---
+++ Pine/Screens/MainCollection/MainCollectionViewCell.swift
@@ -0,0 +1,105 @@
+//
+//  MainCollectionCell.swift
+//  Pine
+//
+//  Created by Nikita Gavrikov on 24.02.2022.
+//
+
+import UIKit
+
+class MainCollectionViewCell: UICollectionViewCell {
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .black
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+
+        imageView.image = UIImage(named: "test")
+        return imageView
+    }()
+
+    private let transparentRectangleView: UIView = {
+        let rectangle = UIView()
+        rectangle.backgroundColor = .black
+        rectangle.alpha = 0.2
+        rectangle.translatesAutoresizingMaskIntoConstraints = false
+        return rectangle
+    }()
+
+    private let firstNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello"
+        label.font = .proTextFontMedium(ofSize: 14)
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let lastNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .proTextFontMedium(ofSize: 14)
+        label.text = "World"
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    private let shareButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "icShareLight"), for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(imageView)
+        imageView.addSubview(transparentRectangleView)
+        imageView.addSubview(firstNameLabel)
+        imageView.addSubview(lastNameLabel)
+        imageView.addSubview(shareButton)
+
+        setConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - SetConstraints
+extension MainCollectionViewCell {
+
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 0),
+            imageView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0),
+            imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0)
+        ])
+
+        NSLayoutConstraint.activate([
+            transparentRectangleView.leftAnchor.constraint(equalTo: self.imageView.leftAnchor, constant: 0),
+            transparentRectangleView.rightAnchor.constraint(equalTo: self.imageView.rightAnchor, constant: 0),
+            transparentRectangleView.bottomAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: 0),
+            transparentRectangleView.topAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: -37)
+
+        ])
+
+        NSLayoutConstraint.activate([
+            firstNameLabel.leftAnchor.constraint(equalTo: self.imageView.leftAnchor, constant: 12),
+            firstNameLabel.bottomAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: -8)
+        ])
+
+        NSLayoutConstraint.activate([
+            lastNameLabel.leftAnchor.constraint(equalTo: self.firstNameLabel.rightAnchor, constant: 5),
+            lastNameLabel.bottomAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: -8)
+        ])
+
+        NSLayoutConstraint.activate([
+            shareButton.rightAnchor.constraint(equalTo: self.imageView.rightAnchor, constant: -5),
+            shareButton.bottomAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: -2)
+        ])
+    }
+}
---
+++ Pine/Screens/MainCollection/MainCollectionViewController.swift
@@ -0,0 +1,115 @@
+//
+//  ViewController.swift
+//  Pine
+//
+//  Created by Nikita Gavrikov on 24.02.2022.
+//
+
+import UIKit
+
+class MainCollectionViewController: UIViewController {
+    private let searchController = UISearchController(searchResultsController: nil)
+    private var imagesCollectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.minimumLineSpacing = 5
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.bounces = false
+        collectionView.register(MainCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.keyboardDismissMode = .onDrag
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+
+    private var imagesData: [Int] = {
+        var massive: [Int] = []
+        for i in 0...20 {
+            massive.append(i)
+        }
+        return massive
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        setDelegate()
+        setConstraints()
+        setNavigationBar()
+        setupSearchController()
+    }
+
+    private func setupViews() {
+        view.backgroundColor = .white
+        view.addSubview(imagesCollectionView)
+    }
+
+    private func setDelegate() {
+        imagesCollectionView.delegate = self
+        imagesCollectionView.dataSource = self
+        searchController.searchBar.delegate = self
+    }
+
+    private func setNavigationBar() {
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+
+    private func setupSearchController() {
+        searchController.searchBar.placeholder = "Search"
+        searchController.obscuresBackgroundDuringPresentation = false
+        let cancelImage = UIImage(named: "icCross")
+        let loupeImage = UIImage(named: "icLoupe")
+        searchController.searchBar.setImage(cancelImage, for: .clear, state: .normal)
+        searchController.searchBar.setImage(loupeImage, for: .search, state: .normal)
+        searchController.searchBar.tintColor = .black
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension MainCollectionViewController: UICollectionViewDataSource {
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imagesData.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MainCollectionViewCell
+        let imageInfo = imagesData[indexPath.row]
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension MainCollectionViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(
+            width: view.bounds.width,
+            height: 270
+        )
+    }
+}
+
+// MARK: - UISearchBarDelegate
+extension MainCollectionViewController: UISearchBarDelegate {
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    }
+}
+
+// MARK: - SetConstraints
+extension MainCollectionViewController {
+
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            imagesCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            imagesCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0),
+            imagesCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0),
+            imagesCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
+        ])
+    }
+}
+
